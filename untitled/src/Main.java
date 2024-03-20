ea0546c6899b2e133d9a2ab5423121a04421c86a
unknown <chintiaputris.17@gmail.com>
2024-03-20 18:53:53 +0700

    FirstClass

---
+++ untitled/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello My Name Chintia Putri Sedayu!");
+        System.out.println("My Age 21");
+        System.out.println("Ini adalah Branch development");
+
+
+        
+        boolean isAlien = false;
+
+        if (isAlien==false){
+            System.out.println("Im  scared of alien");
+            System.out.println("Its no an alien!!");
+        }
+        System.out.println("who i'am?");
+    }
+}
